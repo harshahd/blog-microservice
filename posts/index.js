6b5ec09915b67b0a9780a45a24d2570c83bd3536
harshahd <hdharsha4@gmail.com>
2024-07-16 12:53:14 +0530

    Finished review of post and comment micro services

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const crypto=require('crypto');
 const cors=require('cors');
+const bodyParser=require('body-parser');
 const app=express();
 
 app.use(cors({origin:'*'}));
@@ -8,7 +9,7 @@ app.use(cors({origin:'*'}));
 
 const posts=[];
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 
 app.get("/posts", (req, resp) => {
